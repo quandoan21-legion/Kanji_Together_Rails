baffe03a555f416449ca237fbe301a7ce1c99ced
Babyincar0311 <phamgiakhang0311@gmail.com>
2026-01-21 21:12:37 +0700

    update:application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,20 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  before_action :authenticate_admin!
-  allow_browser versions: :modern
+  # Chỉ yêu cầu đăng nhập đối với các trang không thuộc Devise
+  before_action :authenticate_admin!, unless: :devise_controller?
 
-  # Changes to the importmap will invalidate the etag for HTML responses
+  allow_browser versions: :modern
   stale_when_importmap_changes
+
+  # Cấu hình Layout riêng
+  layout :layout_by_resource
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "devise" # File layout này chúng ta sẽ tạo ở bước 2
+    else
+      "application"
+    end
+  end
 end
